d83aaf55a6b146301d9d5da4838bee7a9ca60f06
ch40x <dasayushqwe@gmail.com>
2023-12-29 20:52:05 +0530

    Read More link add

--- app.js
+++ app.js
@@ -4,22 +4,20 @@ const _ = require("lodash");
 
 const app = express();
 
-app.set('view engine','ejs')
-app.use(express.static("public"))
-app.use(bodyParser.urlencoded({extended : true}))
-
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const posts = [];
 
-
-app.get("/",function(req,res){
-    res.render('home',{
-        posts : posts
-    })
+app.get("/", function (req, res) {
+  res.render("home", {
+    posts: posts,
+  });
 });
 
-app.get("/:page",function(req,res){
-    res.render(req.params.page)
+app.get("/:page", function (req, res) {
+  res.render(req.params.page);
 });
 
 // app.get("/about",function(req,res){
@@ -35,44 +33,37 @@ app.get("/:page",function(req,res){
 //     res.render("post")
 // });
 
+app.post("/compose", function (req, res) {
+  const title = req.body.postTitle;
+  const body = req.body.postContent;
 
-app.post("/compose" , function(req,res){
+  var post = {
+    postTitle: title,
+    postContent: body,
+  };
 
-    const title = req.body.postTitle;
-    const body = req.body.postContent;
+  posts.push(post);
 
-     var post = {
-        postTitle : title,
-        postContent : body
-     }
+  console.log(posts);
 
-     posts.push(post);
-
-     console.log(posts)
-    
-     res.redirect("/")
+  res.redirect("/");
 });
 
-app.get("/posts/:postitle",function(req,res){
+app.get("/posts/:postitle", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postitle);
 
-    const requestedTitle = _.lowerCase(req.params.postitle);
+  for (let i = 0; i < posts.length; i++) {
+    const storedTitle = _.lowerCase(posts[i].postTitle);
 
-    for (let i = 0 ; i < posts.length; i++) {
-
-        const storedTitle = _.lowerCase(posts[i].postTitle);
-
-        if( requestedTitle === storedTitle ){
-    
-            res.render('post',{
-                title : posts[i].postTitle,
-                content : posts[i].postContent
-            });
-
-        }
-
-    };
+    if (requestedTitle === storedTitle) {
+      res.render("post", {
+        title: posts[i].postTitle,
+        content: posts[i].postContent,
+      });
+    }
+  }
 });
 
-app.listen(3000  , function(){
-    console.log("Server is Running on p-3000")
+app.listen(3000 || process.env.PORT, function () {
+  console.log("Server is Running on p-3000");
 });
