ec25a057ab5558d267f1496a0fe19344f697c014
ch40x <dasayushqwe@gmail.com>
2024-01-01 21:57:10 +0545

    Read More Feature Add + Shows Database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const _ = require("lodash");
 const mongoose = require("mongoose");
-const posts = require("./posts");
+const Posts = require("./posts");
 
 const app = express();
 
@@ -11,56 +11,55 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost/blogWeb_db");
-const allPosts = posts.find();
 
-app.get("/", function (req, res) {
-  res.render("home", {
-    posts: allPosts,
-  });
+app.get("/", async (req, res) => {
+  const posts = await Posts.find();
+  res.render("home", { posts });
 });
 
 app.get("/:page", function (req, res) {
   res.render(req.params.page);
 });
 
-
-
 app.post("/compose", function (req, res) {
   const title = req.body.postTitle;
   const body = req.body.postContent;
 
-  main()
+  main();
+
   async function main() {
     console.log("Sucessfully Connected To the Database");
 
     try {
-      const post = await posts.create({
+      const post = await Posts.create({
         postTitle: title,
         postContent: body,
       });
       await post.save().then(() => console.log("Successfully Saved"));
-
-      // console.log(allPosts);
-      
     } catch (error) {
       console.log(error.message);
     }
   }
-  
+
   res.redirect("/");
 });
 
 app.get("/posts/:postitle", function (req, res) {
   const requestedTitle = _.lowerCase(req.params.postitle);
+  readMore()
+  async function readMore() {
+    const allPosts = await Posts.find();
+  
+    for (let i = 0; i < allPosts.length; i++) {
+      const storedTitle = _.lowerCase(allPosts[i].postTitle);
+      console.log(storedTitle);
 
-  for (let i = 0; i < posts.length; i++) {
-    const storedTitle = _.lowerCase(allPosts[i].postTitle);
-
-    if (requestedTitle === storedTitle) {
-      res.render("post", {
-        title: allPosts[i].postTitle,
-        content: allPosts[i].postContent,
-      });
+      if (requestedTitle === storedTitle) {
+        res.render("post", {
+          title: allPosts[i].postTitle,
+          content: allPosts[i].postContent,
+        });
+      }
     }
   }
 });
